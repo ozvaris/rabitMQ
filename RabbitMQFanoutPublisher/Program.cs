4aa761ef982c23591b3f5df6f2017189481ef6d8
Ozgur <ozgurozvaris@hotmail.com>
2022-05-07 12:15:21 +0100

    initial commit

---
+++ RabbitMQFanoutPublisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQFanoutPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnection conn;
+            IModel channel;
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            conn = factory.CreateConnection();
+            channel = conn.CreateModel();
+
+            channel.ExchangeDeclare("ex.fanout", "fanout", true, false, null);
+
+            channel.QueueDeclare("my.queue1", true, false, false, null);
+
+            channel.QueueDeclare("my.queue2", true, false, false, null);
+
+            channel.QueueBind("my.queue1", "ex.fanout", "");
+            channel.QueueBind("my.queue2", "ex.fanout", "");
+
+            channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Message 1"));
+            channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Message 2"));
+
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadKey();
+
+            channel.QueueDelete("my.queue1");
+            channel.QueueDelete("my.queue2");
+            channel.ExchangeDelete("ex.fanout");
+
+            channel.Close();
+            conn.Close();
+
+
+        }
+    }
+}
